b3e1baf8963e14ee7cff72538daae6d25e907c46
TD826 <j7f12d8@yahoo.co.jp>
2021-01-25 14:55:39 +0900

    CSVの読み込みとファイル選択の拡張機能を追加
    追加
    ・CSVの読み込みを行うスクリプト
    ・ファイル選択のウィンドウを開き、特定のファイルを選択させる拡張機能を追加

---
+++ baseProject/Assets/DaigoBase/Extension/FileDesignationAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileDesignationAttribute : PropertyAttribute
+{
+    public enum FILEEXTENSION
+    {
+        NONE = 0,
+        CSV = 1,
+        JSON = 2,
+    }
+    // 拡張子フィルター
+    public FILEEXTENSION extensionFilter = FILEEXTENSION.NONE;
+}
